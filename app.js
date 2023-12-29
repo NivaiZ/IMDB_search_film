a61b16775ad68d1df7e9286aaf97df212ebefc11
IlyaGalyatdinov <i.galyatdinov@yandex.ru>
2023-12-29 20:04:54 +0500

    добавил модальное окно, сделал пагинацию, корректно работает скелетон на загрузку данных, провёл рефакторинг, показал как в идеале нужно типизировать данные, если бы этот код был на TS

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+// В идеале добавить типизацию к получаемым данным
+// interface Movie {
+//   Title: string;
+//   Year: string;
+//   Poster: string;
+//   Type: string;
+// }
+
+// interface SearchResponse {
+//   Search: Movie[];
+//   totalResults: string;
+//   Error?: string;
+// }
 document.addEventListener("DOMContentLoaded", () => {
   const movieName = document.querySelector(".search__input");
   const searchButton = document.querySelector("#search-btn");
   const resultWrapper = document.querySelector("#result");
+  const paginationWrapper = document.querySelector("#pagination");
   const form = document.querySelector("#search__form--film");
   const key = "abfdbba4";
+  const itemsPerPage = 10;
+  let currentPage = 1;
+  let totalResults = 0;
+  let isLoading = false;
 
-  function searchFilmHandler() {
-    const getMovieName = movieName.value;
-    const ApiUrl = `https://www.omdbapi.com/?s=${getMovieName}&apikey=${key}`;
-
-    if (getMovieName.length <= 0) {
-      resultWrapper.innerHTML = `<h3 class="msg">Введите название фильма</h3>`;
+  function searchFilmHandler(page = 1) {
+    if (isLoading) {
+      return;
     }
+    const getMovieName = movieName.value.trim("");
+    const ApiUrl = `http://www.omdbapi.com/?s=${getMovieName}&apikey=${key}&page=${page}&plot=full`;
+
+    if (getMovieName.length === 0) {
+      resultWrapper.innerHTML = `<h3 class="msg">Введите название фильма</h3>`;
+      return;
+    }
+    isLoading = true;
     showLoadingIndicator();
+
     fetch(ApiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -23,31 +47,48 @@ document.addEventListener("DOMContentLoaded", () => {
         return response.json();
       })
       .then((data) => {
-        hideLoadingIndicator();
         if (data.Error) {
           resultWrapper.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
-        } else if (data.Search && data.Search.length > 0) {
-          resultWrapper.innerHTML = data.Search.map((movie) => {
-            return `
-              <div class="info">
-                <img src=${movie.Poster} class="poster">
-                <div>
-                    <h2>${movie.Title}</h2>
-                    <div class="details">
-                        <span>${movie.Year}</span>
-                        <span>${movie.Type}</span>
-                    </div>
-                </div>
-              </div>
-            `;
-          }).join("");
         } else {
-          resultWrapper.innerHTML = `<h3 class="msg">Фильмы не найдены</h3>`;
+          totalResults = parseInt(data.totalResults);
         }
+        const detailedRequests = data.Search.map((movie) =>
+          fetch(
+            `http://www.omdbapi.com/?i=${movie.imdbID}&apikey=${key}&plot=full`
+          )
+            .then((response) => response.json())
+            .then((detailedData) => {
+              movie.Plot = detailedData.Plot;
+              return movie;
+            })
+            .catch((error) => {
+              console.error(
+                "Ошибка при получении дополнительных данных:",
+                error
+              );
+              return movie;
+            })
+        );
+        Promise.all(detailedRequests)
+          .then((updatedMovies) => {
+            renderMovies(updatedMovies);
+            renderPagination();
+            initModal();
+          })
+          .catch((error) =>
+            console.error("Ошибка при ожидании завершения запросов:", error)
+          )
+          .finally(() => {
+            isLoading = false;
+            hideLoadingIndicator(); // Перемещено сюда
+          });
       })
       .catch((error) => {
         console.error("Ошибка при получении данных:", error);
-        resultWrapper.innerHTML = `<h3 class="msg">Произошла ошибка при получении данных</h3>`;
+        resultWrapper.innerHTML = `<h3 class="msg">Произошла ошибка при получении данных. Временные работы на сервере. Приносим извинения за предоставленные неудобства!</h3>`;
+        paginationWrapper.innerHTML = "";
+        isLoading = false;
+        hideLoadingIndicator(); // Перемещено сюда
       });
   }
 
@@ -62,9 +103,88 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function renderMovies(movies) {
+    resultWrapper.innerHTML = movies
+      .map((movie) => {
+        console.log(movie);
+        return `
+          <div class="info">
+            <img src=${movie.Poster} class="poster">
+            <div class="info__wrapper">
+              <h2>${movie.Title}</h2>
+              <div class="details">
+                <span>${movie.Year}</span>
+                <span>${movie.Type}</span>
+              </div>
+              <button class="button modal__information" data-micromodal-trigger="modal-${
+                movie.imdbID
+              }">
+                <span class="modal__signature">Details</span>
+              </button>
+      
+              <div class="modal micromodal-slide" id="modal-${
+                movie.imdbID
+              }" aria-hidden="true">
+                <div class="modal-overlay" tabindex="-1" data-micromodal-close>
+                  <div class="modal-container" role="dialog" aria-modal="true" aria-labelledby="modal-${
+                    movie.imdbID
+                  }-title">
+                    <header class="modal-header">
+                      <h2 class="modal-title" id="modal-${
+                        movie.imdbID
+                      }-title">Micromodal</h2>
+                      <button class="modal-close" aria-label="Close modal" data-micromodal-close></button>
+                    </header>
+                    <div class="modal-content" id="modal-${
+                      movie.imdbID
+                    }-content">
+                      <p class="modal-text">${
+                        movie.Plot || "Информация отсутствует"
+                      }</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+      })
+      .join("");
+  }
+
+  function initModal() {
+    MicroModal.init({
+      disableScroll: true,
+      awaitOpenAnimation: true,
+      awaitCloseAnimation: true,
+    });
+  }
+
+  function renderPagination() {
+    const totalPage = Math.ceil(totalResults / itemsPerPage);
+
+    paginationWrapper.innerHTML = Array.from(
+      { length: totalPage },
+      (_, index) => index + 1
+    )
+      .map(
+        (PageNumber) =>
+          `<li class="pagination__item swiper-slide"><button class="pagination-btn" data-page="${PageNumber}" type="button">${PageNumber}</button></li>`
+      )
+      .join("");
+
+    const paginationButtons = document.querySelectorAll(".pagination-btn");
+    paginationButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const page = parseInt(button.dataset.page);
+        currentPage = page;
+        searchFilmHandler(page);
+      });
+    });
+  }
+
   form.addEventListener("submit", (event) => {
     event.preventDefault();
   });
-
-  searchButton.addEventListener("click", searchFilmHandler);
+  searchButton.addEventListener("click", () => searchFilmHandler(currentPage));
 });
